b154d1e5b845aa03e4d93f0f493aec81d9eebd70
keshav-sudo <thesharmakeshav@gmail.com>
2025-09-17 16:22:42 +0530

    Folder Structure Resolving

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import redisClient from "./config/redisClient.js";
 import type { Request, Response } from "express";
-import cookieParser from "cookie-parser";
 import http from "http";
 
 import authRoutes from "./routes/user.route.js"
@@ -9,18 +8,15 @@ import authRoutes from "./routes/user.route.js"
 const app = express();
 const PORT = 5000;
 
-app.use(cookieParser());
+
 app.use(express.json());
 app.use("/api/v1/auth" , authRoutes);
 
-app.get('/' , (req : Request, res : Response) => {
-    res.send("Your Server Is on");
+app.get('/' , (res : Response) => {
+    res.send("Your Server Is on 5000");
 })
 
-app.get("/read-cookie", (req, res) => {
-  const token = req.cookies.token; 
-  res.send(`Token: ${token}`);
-});
+
 
 const server:http.Server = app.listen( PORT , ()=> {
      console.log(`Server is running on http://localhost:${PORT}`);
---
+++ services/gateway/dist/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import { setupRoutes } from "./routes/routes.js";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+setupRoutes(app);
+app.get("/", (req, res) => {
+    res.send("API Gateway is running 🚀");
+});
+app.listen(PORT, () => {
+    console.log(`API Gateway listening on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
